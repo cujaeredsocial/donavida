43318106c63429017cf1b70b2101a693043055d6
usagi03 <117960829+usagi03@users.noreply.github.com>
2023-08-03 17:15:25 -0400

    actualizacion de la gestion

---
+++ backend/vue/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-app>
+    <div id="app">
+      <v-toolbar app color="#fe3b3b" dark>
+        <v-app-bar-nav-icon></v-app-bar-nav-icon>
+        <v-toolbar-title><h1>DonaVida</h1></v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn v-if="$route.name === 'register'" text @click="toInicio">
+          <v-icon>mdi mdi-login-variant</v-icon>
+          <span class="mr-2">Inicia Sesion</span>
+        </v-btn>
+        <v-btn v-else-if="$route.name === 'login'" text @click="toRegister">
+          <v-icon>mdi mdi-account-plus-outline</v-icon>
+          <span class="mr-2">Registrar Sesion</span>
+        </v-btn>
+      </v-toolbar>
+      <v-main>
+        <router-view />
+      </v-main>
+    </div>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: "App",
+  components: {},
+  data() {
+    return {};
+  },
+
+  methods: {
+    toSolicitud() {
+      this.$router.push("/solicitud");
+    },
+    toInicio() {
+      this.$router.push("/login");
+    },
+    toRegister() {
+      this.$router.push("/register");
+    },
+  },
+};
+</script>
---
+++ backend/vue/src/components/LogUp.vue
@@ -0,0 +1,71 @@
+<template>
+  <v-container class="text-center">
+    <v-card class="mx-auto my-12" width="400" height="450">
+      <v-col class="mx-auto" cols="12" md="10">
+        <!--Logo de DonaVida-->
+        <v-img
+          :src="require('../assets/DonaVida-removebg.png')"
+          class="my-3"
+          contain
+          height="90"
+        />
+        <!--Usuario-->
+        <v-text-field
+          v-model="user.username"
+          :rules="rules.name"
+          label="Usuario"
+          placeholder="Introduzca su Usuario"
+          required
+        ></v-text-field>
+        <!--Correo-->
+        <v-text-field
+          v-model="user.email"
+          :rules="rules.name"
+          label="Correo"
+          placeholder="Introduzca su Correo"
+          required
+        ></v-text-field>
+        <!--Contraseña-->
+        <v-text-field
+          v-model="user.password"
+          :rules="rules.name"
+          :append-icon="!show ? 'mdi-eye' : 'mdi-eye-off'"
+          :type="show ? 'text' : 'password'"
+          label="Contraseña"
+          placeholder="Introduzca su contraseña"
+          @click:append="show = !show"
+          required
+        ></v-text-field>
+        <!--Boton de Inicio-->
+        <v-btn class="ma-2 white--text" color="red">
+          <v-icon>mdi mdi-account-plus-outline</v-icon>
+          <span>Registrar</span>
+        </v-btn>
+      </v-col>
+      <v-col class="mx-auto" cols="12" md="10">
+        <span>¿Ya tienes una cuenta? </span>
+        <router-link to="/login" style="cursor: pointer">
+          Inicia Sesion
+        </router-link>
+      </v-col>
+    </v-card>
+  </v-container>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      show: false,
+      valid: false,
+      user: {
+        username: "",
+        password: "",
+        email: "",
+      },
+      rules: {
+        name: [(val) => (val || "").length > 0 || "No ha llenado este campo"],
+      },
+    };
+  },
+};
+</script>
