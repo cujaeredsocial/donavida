608cf22b0eb12e3739a4848fdaa0917e639d4df0
FaboReyes <faboale002@gmail.com>
2023-08-03 20:16:23 -0400

    Descomente las rutas de los metodos post

--- backend/api/routes/login.js
+++ backend/api/routes/login.js
@@ -7,11 +7,11 @@ const userController = require("../controllers/user");
 const router = express.Router();
 
 router.post("/createuser", userController.postCreateUser);
-//  router.post('/user', userController.postReedUser);
+router.post('/user', userController.postReedUser);
 router.post("/updateuser", userController.postUpdateUser);
-//  router.post('/allusers', userController.postAllUsers);
+router.post('/allusers', userController.postAllUsers);
 router.post("/donorssusers", userController.postAllDonorsUsers);
-//  router.post('/managersuser', userController.postAllManagersUsers);
+router.post('/managersuser', userController.postAllManagersUsers);
 router.post("/deleteuser", userController.postDeleteUser);
 
 module.exports = router;
