3783b86f48e721bf831bf59c6b9375565b7cb19d
AfroAg02 <afro.aguirre1969@gmail.com>
2023-08-05 03:34:57 -0400

    Modelo de usuario con token

--- backend/api/models/user.js
+++ backend/api/models/user.js
@@ -1,29 +1,27 @@
 const mongoose = require('mongoose');
 
+
 const userSchema = new mongoose.Schema({
-  userName: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true,
-  },
- // manager: {
- //   type: Boolean,
- //   required: true,    
- // },
-  password: {
-    type: String,
-    required: true,
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-});
+    userName: {
+        type:String,
+        required:true,
+        minLength:[4,'Name should be minimum of 4 characters']
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+    },
+    password:{
+        type:String,
+        required:true,
+        minLength:[8,'Password should be minimum of 8 characters']
+    },
+    token:{
+        type:String
+    }
+})
 
-const User = mongoose.model('User', userSchema);
 
-module.exports = User;
+const userModel = mongoose.model('user',userSchema);
+module.exports = userModel;
