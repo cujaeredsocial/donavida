f0bdbd2db632f3c53f898ee500426d9ab9298540
AfroAg02 <afro.aguirre1969@gmail.com>
2023-08-05 03:35:30 -0400

    rutas actualizadas

--- backend/api/routes/login.js
+++ backend/api/routes/login.js
@@ -1,17 +1,16 @@
-const path = require("path");
-
 const express = require("express");
-
+const isAuthenticated = require('../middleware/authenticate');
 const userController = require("../controllers/user");
 
 const router = express.Router();
 
 router.post("/createuser", userController.postCreateUser);
-router.post('/user', userController.postReedUser);
+router.post('/login', userController.postAuthenticateUser);
 router.post("/updateuser", userController.postUpdateUser);
-router.post('/allusers', userController.postAllUsers);
+router.get('/allusers', userController.postAllUsers);
 //router.post("/donorssusers", userController.postAllDonorsUsers);
 //router.post('/managersuser', userController.postAllManagersUsers);
 router.post("/deleteuser", userController.postDeleteUser);
+
 
 module.exports = router;
