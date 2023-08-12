a9818291fa3559542c13a9ac007ddae2f660bebd
usagi03 <jmspg2017@gmail.com>
2023-08-11 22:04:12 -0400

    actualizado el rol del gestor

--- backend/vue/src/components/templates/RolGestor.vue
+++ backend/vue/src/components/templates/RolGestor.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container class="spacing-playground pa-12" fluid>
-    <v-card class="mx-auto my-auto" width="800" height="450">
+    <v-card class="mx-auto my-auto" width="800" min-height="450">
       <!--logo de donavida-->
       <v-img
         :src="require('../../assets/DonaVida-removebg.png')"
@@ -8,73 +8,80 @@
         contain
         height="60"
       />
-      <v-form>
-        <!--Los campos ya guardados en la base datos-->
-        <v-col v-for="(campo, index) in camposArray" :key="index" class="ms-8">
-          <v-row>
-            <v-col cols="12" md="3">
-              <v-text-field
-                label="Titulo"
-                v-model="camposArray[index].title"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" md="2">
-              <v-text-field
-                label="Tipo"
-                v-model="camposArray[index].type"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" md="2">
-              <v-text-field
-                label="Valor"
-                v-model="camposArray[index].value"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" md="4">
-              <v-text-field
-                label="Regex"
-                v-model="camposArray[index].regex"
-              ></v-text-field>
-            </v-col>
-          </v-row>
-        </v-col>
-        <!--Los campos nuevos que va a introducir el usuario-->
-        <v-col v-for="(campo, index) in n" :key="index" class="ms-8">
-          <v-row>
-            <v-col cols="12" md="3">
-              <v-text-field
-                label="Titulo"
-                v-model="component.title"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" md="2">
-              <v-text-field
-                label="Tipo"
-                v-model="component.type"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" md="2">
-              <v-text-field
-                label="Valor"
-                v-model="component.value"
-              ></v-text-field>
-            </v-col>
-            <v-col cols="12" md="4">
-              <v-text-field
-                label="Regex"
-                v-model="component.regex"
-              ></v-text-field>
-            </v-col>
-          </v-row>
-        </v-col>
-        <v-card-actions>
-          <v-col class="ms-6" cols="12" md="10">
-            <v-btn color="red" @click="nuevoCampo" text>
-              <v-icon> MAS</v-icon>
-            </v-btn>
+      <v-card-text>
+        <v-form>
+          <!--Los campos ya guardados en la base datos-->
+          <v-col
+            v-for="(campo, index) in camposArray"
+            :key="index"
+            class="ms-8 mt-n9"
+          >
+            <v-row>
+              <v-col cols="12" md="3">
+                <v-text-field
+                  label="Titulo"
+                  v-model="camposArray[index].title"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" md="2">
+                <v-text-field
+                  label="Tipo"
+                  v-model="camposArray[index].type"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" md="2">
+                <v-text-field
+                  label="Valor"
+                  v-model="camposArray[index].value"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" md="3">
+                <v-text-field
+                  label="Regex"
+                  v-model="camposArray[index].regex"
+                ></v-text-field>
+              </v-col>
+            </v-row>
           </v-col>
-        </v-card-actions>
-      </v-form>
+          <!--Los campos nuevos que va a introducir el usuario-->
+          <v-col v-for="(campo, index) in n" :key="index" class="ms-8 mt-n8">
+            <v-row>
+              <v-col cols="12" md="3">
+                <v-text-field
+                  label="Titulo"
+                  v-model="component.title"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" md="2">
+                <v-text-field
+                  label="Tipo"
+                  v-model="component.type"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" md="2">
+                <v-text-field
+                  label="Valor"
+                  v-model="component.value"
+                ></v-text-field>
+              </v-col>
+              <v-col cols="12" md="4">
+                <v-text-field
+                  label="Regex"
+                  v-model="component.regex"
+                ></v-text-field>
+              </v-col>
+            </v-row>
+          </v-col>
+        </v-form>
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-actions>
+        <v-btn color="green" @click="nuevoCampo" outlined rounded icon>
+          <v-icon>mdi mdi-plus</v-icon>
+        </v-btn>
+      </v-card-actions>
     </v-card>
   </v-container>
 </template>
