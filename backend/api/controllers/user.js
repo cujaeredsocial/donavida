ed6615e8565b02a503ae1f72c1e71d298eb02357
AfroAg02 <afro.aguirre1969@gmail.com>
2023-08-21 09:18:29 -0400

    Comentando lo que hizo fabian

--- backend/api/controllers/user.js
+++ backend/api/controllers/user.js
@@ -155,7 +155,7 @@ exports.getUser = (req, res) => {
 
 //Fabian asignar rol y llenar datos
 exports.AsignarRol = (req,res) =>{
-  const{userName,rol,components} = req.body;
+  let{userName,rol,components} = req.body;
   if(!user || !rol || !components){
     return res.status(401).json({message: 'Error en la introduccion de datos'});
   }
--- backend/api/models/user.js
+++ backend/api/models/user.js
@@ -29,11 +29,11 @@ const userSchema = new mongoose.Schema({
         type:Boolean,
         default:true
     },
-    datos_roles :[{//aca va un arreglo con el rol y los componentes
+    datos_roles :{//aca va un arreglo con el rol y los componentes
         rol :String,
-        components:[Component.Schema]
+        components: [Component.schema] 
     }
-    ]
+    
 });
 
 
