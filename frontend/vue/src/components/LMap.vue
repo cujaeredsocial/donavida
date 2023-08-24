a12e5661da0e7e9bfd3c6dfef08c62082c054193
frankxhunter <frankxhunter@gmail.com>
2023-08-24 10:14:03 -0400

    marcador en el mapa

--- frontend/vue/src/components/LMap.vue
+++ frontend/vue/src/components/LMap.vue
@@ -2,15 +2,18 @@
     <v-card style="background-color:rgb(255, 0, 0, 0.3); color:white" outlined>
       <v-card-title class="white-text">Seleccione la Ubicacion de su donacion</v-card-title>
       <v-card-text>
-        <l-map :zoom="zoom" :center="center" style="height: 500px; z-index: 0;" @click="showLatLng">
+        <l-map :zoom="zoom" :center="center" style="height: 500px; z-index: 0;" @click="addMarker">
           <l-tile-layer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"></l-tile-layer>
+          <l-marker v-if="marker!=null" :lat-lng="marker.latLng" :key="marker.id" >
+            <l-icon :icon-url="url" :icon-size="[32,32]"></l-icon>
+          </l-marker>
         </l-map>
       </v-card-text>
     </v-card>
   </template>
   
 <script>
-import { LMap, LTileLayer, LMarker} from 'vue2-leaflet';
+import { LMap, LTileLayer, LMarker, LIcon} from 'vue2-leaflet';
 import 'leaflet/dist/leaflet.css';
 
 export default {
@@ -18,7 +21,14 @@ export default {
   components: {
     LMap,
     LTileLayer,
-    LMarker
+    LMarker,
+    LIcon,
+  },
+  data(){
+    return{
+      marker: null,
+      url: "https://iconos8.es/icon/13800/marcador",
+    }
   },
   methods: {
     showLatLng(e) {
@@ -27,6 +37,15 @@ export default {
     var lng = coord.lng;
     console.log("Has hecho clic en el mapa en la latitud: " + lat + " y longitud: " + lng);
    },
+   addMarker(event) {
+    const latLng = event.latlng;
+    const marker = {
+      id: Date.now(),
+      latLng: [latLng.lat, latLng.lng],
+    };
+    this.marker=marker;
+    console.log(this.marker)
+  },
 },
   props: {
     zoom: {
