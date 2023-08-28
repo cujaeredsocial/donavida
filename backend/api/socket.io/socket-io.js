e73af6a225c42d700eb76f967e0deb72daf0f562
FaboReyes <faboale002@gmail.com>
2023-08-28 10:29:06 -0400

    Eliminando comentarios

--- backend/api/socket.io/socket-io.js
+++ backend/api/socket.io/socket-io.js
@@ -14,7 +14,6 @@ exports.socketConnection = server => {
    // 1-Enviar ademas de el metauser un objeto socket al metodo con el que se emitiran las notificacioines en vez
     //de enviar una respuesta
     socket.on("Solicitud",(metauser) =>{
-      //postCrear(metauser,socket.handshake.res);// este es un modo para trabajar con la res pero hay que actualizar socket
       postCrear(metauser,socket);
     })
     socket.on("disconnect", () => {
@@ -28,26 +27,3 @@ exports.SimpleEmit = (name, message) => {
  
   };
 
-//emicion de eventos
-
-/**
- * //1-inicializar la web socket
-const io = new Server(server);
-
-//emicion de eventos
-io.emit(() =>{
-  console.log('Usted esta conectado');//simple llamada para ver que socket.io este instalado y funcionando
-});
-//Multiplexado solo para el uso de los admins
-const admins = io.of('/admin');//esto sirve para las opciones de los gestores
-
-admins.emit(() => {
-  console.log("Hola soy admin");
-})
-//2-escuchar eventos
-io.on('connection',(socket) =>{
-  sockett.send.json('ho;a')
-  console.log("hola mundo");
-  console.log('Gracias por utilizar nuestra red');
-});
- */
